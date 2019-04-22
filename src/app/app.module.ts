6f299c64d504252b17a3b0a607d87c31533de9a1
Nikola Radovic <dzona065@gmail.com>
2019-04-22 17:59:42 +0200

    material components imported in main app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatTableModule} from '@angular/material/table';
 
 import { AppComponent } from './app.component';
+import { CryptocurrencyListComponent } from './cryprocurrencies/cryptocurrency-list.component';
+import { CryptocurrencyService } from 'src/providers/cryptocurrency/cryptocurrency-service';
+import { Api } from 'src/providers/api/api';
+import { TokenInterceptor } from 'src/providers/api/token.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CryptocurrencyListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
-  providers: [],
+  providers: [
+    Api,
+    CryptocurrencyService,
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
