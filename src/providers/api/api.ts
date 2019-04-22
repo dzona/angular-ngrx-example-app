fbaf0d1dc816b3ab9420bc64b71b5745ef6c6055
Nikola Radovic <dzona065@gmail.com>
2019-04-22 17:58:23 +0200

    api service added
    service that all other data providers that fetch data from marketcap will use,
    so they don't have to configure usual stuff, such as endpoint base url,
    params...

---
+++ src/providers/api/api.ts
@@ -0,0 +1,50 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import environment from './../../environments/environment';
+
+
+/**
+ * Api is a generic REST Api handler. Set your API url first.
+ */
+@Injectable()
+export class Api {
+  url: string = `${environment.apiUrl}/v1`;
+
+  constructor(public http: HttpClient) {
+  }
+
+  get(endpoint: string, params?: any, reqOpts?: any): Observable<any> {
+    if (!reqOpts) {
+      reqOpts = {
+        params: new HttpParams()
+      };
+    }
+
+    if (params) {
+      reqOpts.params = new HttpParams();
+      for (let k in params) {
+        reqOpts.params = reqOpts.params.set(k, params[k]);
+      }
+    }
+
+    return this.http.get(this.url + '/' + endpoint, reqOpts);
+  }
+
+  post(endpoint: string, body: any, reqOpts?: any): Observable<any> {
+    return this.http.post(this.url + '/' + endpoint, body, reqOpts);
+  }
+
+  put(endpoint: string, body: any, reqOpts?: any): Observable<any> {
+    return this.http.put(this.url + '/' + endpoint, body, reqOpts);
+  }
+
+  delete(endpoint: string, reqOpts?: any): Observable<any> {
+    return this.http.delete(this.url + '/' + endpoint, reqOpts);
+  }
+
+  patch(endpoint: string, body: any, reqOpts?: any): Observable<any> {
+    return this.http.patch(this.url + '/' + endpoint, body, reqOpts);
+  }
+
+}
