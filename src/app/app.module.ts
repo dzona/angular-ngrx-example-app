89f63c313da55f66b6146706085da24e8523d878
Nikola Radovic <dzona065@gmail.com>
2019-04-23 17:08:16 +0200

    app module updated for new pages, and added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatNativeDateModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { MatNativeDateModule, MatPaginatorModule, MatSortModule, MatLabel, MatFormFieldModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { CryptocurrencyListComponent } from './cryprocurrencies/cryptocurrency-list.component';
-import { TokenInterceptor } from 'src/providers/api/token.interceptor';
+import { CryptocurrencyListComponent } from './components/cryprocurrency-list/cryptocurrency-list.component';
+import { TokenInterceptor } from 'src/app/providers/api/token.interceptor';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { CryptocurrencyDetailsComponent } from './components/cryptocurrency-details/cryptocurrency-details.component';
+import { CryptocurrencySettingsComponent } from './components/cryptocurrency-settings/cryptocurrency-settings.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'settings', component: CryptocurrencySettingsComponent },
+  { path: 'cryptocurrencies', component: CryptocurrencyListComponent },
+  { path: 'cryptocurrencies/:id', component: CryptocurrencyDetailsComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CryptocurrencyListComponent,
+    PageNotFoundComponent,
+    CryptocurrencyDetailsComponent,
+    CryptocurrencySettingsComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,7 +44,9 @@ import { TokenInterceptor } from 'src/providers/api/token.interceptor';
     MatProgressSpinnerModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    MatFormFieldModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
