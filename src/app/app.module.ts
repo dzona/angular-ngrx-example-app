193cb3ff58f8c55d9f9ac3f3885007d7668d3934
Nikola Radovic <dzona065@gmail.com>
2019-04-26 00:42:00 +0200

    main module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatNativeDateModule, MatPaginatorModule, MatSortModule, MatLabel, MatFormFieldModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatTableModule } from '@angular/material/table';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { CryptocurrencyListComponent } from './components/cryprocurrency-list/cryptocurrency-list.component';
 import { TokenInterceptor } from 'src/app/providers/api/token.interceptor';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { CryptocurrencyDetailsComponent } from './components/cryptocurrency-details/cryptocurrency-details.component';
-import { CryptocurrencySettingsComponent } from './components/cryptocurrency-settings/cryptocurrency-settings.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CryptocurrencyModule } from './cryptocurrency/cryptocurrency.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import environment from './../environments/environment';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'settings', component: CryptocurrencySettingsComponent },
-  { path: 'cryptocurrencies', component: CryptocurrencyListComponent },
-  { path: 'cryptocurrencies/:id', component: CryptocurrencyDetailsComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
@@ -28,25 +23,22 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    CryptocurrencyListComponent,
     PageNotFoundComponent,
-    CryptocurrencyDetailsComponent,
-    CryptocurrencySettingsComponent,
     DashboardComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule,
     HttpClientModule,
-    MatNativeDateModule,
-    ReactiveFormsModule,
-    MatProgressSpinnerModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatFormFieldModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    CryptocurrencyModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      name: 'Cryptocurrency Stock',
+      maxAge: 25,
+      logOnly: environment.production
+    })
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
