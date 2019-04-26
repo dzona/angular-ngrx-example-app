b2e34b1dd7fda71c269332443a6e5790a61e1f3e
Nikola Radovic <dzona065@gmail.com>
2019-04-26 23:54:58 +0200

    NgRx Effect update not to send Http request if grid data is available

--- src/app/cryptocurrency/cryptocurrency-store/cryptocurrency.effects.ts
+++ src/app/cryptocurrency/cryptocurrency-store/cryptocurrency.effects.ts
@@ -2,33 +2,43 @@ import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { CryptocurrencyService } from 'src/app/providers/cryptocurrency/cryptocurrency-service';
 import * as cryptocurrencyActions from './cryptocurrency.actions';
-import { mergeMap, map, catchError } from 'rxjs/operators';
+import { map, catchError, withLatestFrom, filter, switchMap, share } from 'rxjs/operators';
 import { ApiResponse } from 'src/app/models/api-response';
 import { of as observableOf } from 'rxjs';
+import { Store, select } from '@ngrx/store';
 
 @Injectable()
 export class CryptocurrencyEffects {
 
     constructor(
         private actions$: Actions,
+        private store: Store<any>,
         private service: CryptocurrencyService
     ) { }
 
     @Effect()
     loadCryptocurrencies = this.actions$.pipe(
         ofType(cryptocurrencyActions.CryptocurrencyActionTypes.CryptocurrencyListLoad),
-        mergeMap((action: cryptocurrencyActions.CryptocurrencyListLoaded) => this.service.getAll(action.payload).pipe(
-            map(
-                response => {
-                    let apiResponse = new ApiResponse(response);
-                    let cacheKey = JSON.stringify(action.payload);
+        withLatestFrom(this.store.pipe(select('cryptocurrency'))),
+        filter(([action, store]) => {
+            let cacheKey = JSON.stringify(action.payload);
+            return !(cacheKey in store.cryptocurrencies);
+        }),
+        switchMap(([action, store]) => {
+            return this.service.getAll(action.payload).pipe(
+                map(
+                    response => {
+                        let apiResponse = new ApiResponse(response);
+                        let cacheKey = JSON.stringify(action.payload);
 
-                    return apiResponse.isSuccess ?
-                        new cryptocurrencyActions.CryptocurrencyListLoaded({ key: cacheKey, data: apiResponse.data }) :
-                        new cryptocurrencyActions.CryptocurrencyListLoadFailed(`code: ${apiResponse.status.error_code}; message: ${apiResponse.status.error_message}`);
-                }
-            ),
-            catchError(err => observableOf(new cryptocurrencyActions.CryptocurrencyListLoadFailed(err)))
-        ))
+                        return apiResponse.isSuccess ?
+                            new cryptocurrencyActions.CryptocurrencyListLoaded({ key: cacheKey, data: apiResponse.data, totalRecords: apiResponse.getTotalCount()}) :
+                            new cryptocurrencyActions.CryptocurrencyListLoadFailed(`code: ${apiResponse.status.error_code}; message: ${apiResponse.status.error_message}`);
+                    }
+                ),
+                catchError(err => observableOf(new cryptocurrencyActions.CryptocurrencyListLoadFailed(err)))
+            )
+        }
+        ), share()
     )
 }
